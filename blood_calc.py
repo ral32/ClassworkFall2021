c5d1569e50bf9cac128f7b1e95ba941a15625632
Rachel Lopez <rachel.lopez@duke.edu>
2021-09-02 12:15:41 -0400

    Fixed incorrect syntax: I had a function call on the lefthand side of an equals sign when it should have been a variable name.

--- blood_calc.py
+++ blood_calc.py
@@ -9,7 +9,7 @@ def interface():
 		return choice
 		if choice == 9:
 			keep_running = False
-		elif chouce == 1:
+		elif choice == 1:
 			HDL_Driver()
 
 
@@ -37,9 +37,31 @@ def hdl_analysis():
 		return "Low"
 
 def hdl_output(HDL_value, HDL_answer):
-	print("The HDL value of {} is considered {}" .format(HDL_value, HDL_answer))
+	print("The HDL value of {} is considered {}". format(HDL_value, HDL_answer))
+	
+
+def LDL_Driver():
+	LDL_value = ldl_input()
+	LDL_character = ldl_analysis(LDL_value)
+	ldl_output()
+
+def ldl_input():
+	ldl_value = int(input("Enter LDL Value: "))
+	return ldl_value
+
+def ldl_analysis():
+	if LDL_value < 130:
+		return "Normal"
+	elif 130 <= LDL_value < 159:
+		return "Borderline High"
+	elif 160 <= LDL_value <= 189:
+		return "High"
+	else:
+		return "Very High"
+
+	def ldl_output(LDL_value, LDL_answer):
+		print("The LDL value of {} is considered {}". format(LDL_value, LDL_answer))
+
 	
 
 
-
-interface()
